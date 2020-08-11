ed4fe11aa25538400c59cb3050c957ac31683542
unknown <ridamsaxena90@gmail.com>
2020-08-11 14:56:34 +0530

    secured the firebase  key.

--- src/backend/firebase.js
+++ src/backend/firebase.js
@@ -1,15 +1,7 @@
-import firebase from 'firebase';
+import firebase from "firebase";
+import { FIREBASE_CONFIG } from "../config";
 
-const firebaseConfig = {
-	apiKey: 'AIzaSyBoxtRcvBWCMfjHmPqXWbEJSW62G0Lq_-4',
-	authDomain: 'instagram-clone-react-370ba.firebaseapp.com',
-	databaseURL: 'https://instagram-clone-react-370ba.firebaseio.com',
-	projectId: 'instagram-clone-react-370ba',
-	storageBucket: 'instagram-clone-react-370ba.appspot.com',
-	messagingSenderId: '337341151354',
-	appId: '1:337341151354:web:262a53388c2600aec2a3c3',
-	measurementId: 'G-3HDX3EVH54'
-};
+const firebaseConfig = FIREBASE_CONFIG;
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
